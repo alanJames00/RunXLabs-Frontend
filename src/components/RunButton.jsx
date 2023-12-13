751f0dcc7b67cd047267162785fb97229c006a53
alanjames00 <alanjames6282@gmail.com>
2023-12-13 22:54:06 +0530

    create run btn comp

---
+++ src/components/RunButton.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function RunButton() {
+
+    return (
+        <div>
+        <Button variant="contained" size='large'>&gt; Run</Button>
+        </div>       
+    );
+}
