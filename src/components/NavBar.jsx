69e7fb774e3c801b8fbbe4d08b162268c74d60dd
alanjames00 <alanjames6282@gmail.com>
2023-12-13 20:58:17 +0530

    create navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+
+export default function NavBar(){
+
+    return (
+        <div className=" h-16 bg-slate-500">
+            <button>Run</button>
+            <button>Run</button>
+            <button>Run</button>
+        </div>
+    );
+}
