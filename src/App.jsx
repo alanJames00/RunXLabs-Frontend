b139b505e7160d2406592c4259ff7e0471c5cf90
alanjames00 <alanjames6282@gmail.com>
2023-12-13 20:52:10 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import CodeEditor from './components/CodeEditor'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello world</h1>
+      <CodeEditor />
     </>
   )
 }
