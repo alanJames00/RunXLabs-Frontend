d898eef41e47f7c7653d8e5e499445ab91eed0e6
alanjames00 <alanjames6282@gmail.com>
2023-12-13 22:47:44 +0530

    add langarray to select runtimes

--- src/components/RuntimeDropdown.jsx
+++ src/components/RuntimeDropdown.jsx
@@ -5,24 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 
 export default function RuntimeDropdown() {
     
-    const currencies = [
-        {
-          value: 'USD',
-          label: '$',
-        },
-        {
-          value: 'EUR',
-          label: '€',
-        },
-        {
-          value: 'BTC',
-          label: '฿',
-        },
-        {
-          value: 'JPY',
-          label: '¥',
-        },
-      ];
+    const langArray = [{name:'javascript', val:'js'}, {name:'python',val:'py'}, {name:'c',val:'c'}]
 
     return (
 
@@ -36,98 +19,16 @@ export default function RuntimeDropdown() {
       >
         <div>
           <TextField
-            id="outlined-select-currency"
+            id="outlined-select-language"
             select
-            label="Select"
-            defaultValue="EUR"
-            helperText="Please select your currency"
+            label="Runtime"
+            defaultValue="js"
+            helperText="Select the language"
           >
-            {currencies.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
+            {langArray.map((option) => (
+              <MenuItem key={option.va} value={option.val}>
+                {option.name}
               </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            id="outlined-select-currency-native"
-            select
-            label="Native select"
-            defaultValue="EUR"
-            SelectProps={{
-              native: true,
-            }}
-            helperText="Please select your currency"
-          >
-            {currencies.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </TextField>
-        </div>
-        <div>
-          <TextField
-            id="filled-select-currency"
-            select
-            label="Select"
-            defaultValue="EUR"
-            helperText="Please select your currency"
-            variant="filled"
-          >
-            {currencies.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            id="filled-select-currency-native"
-            select
-            label="Native select"
-            defaultValue="EUR"
-            SelectProps={{
-              native: true,
-            }}
-            helperText="Please select your currency"
-            variant="filled"
-          >
-            {currencies.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </TextField>
-        </div>
-        <div>
-          <TextField
-            id="standard-select-currency"
-            select
-            label="Select"
-            defaultValue="EUR"
-            helperText="Please select your currency"
-            variant="standard"
-          >
-            {currencies.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            id="standard-select-currency-native"
-            select
-            label="Native select"
-            defaultValue="EUR"
-            SelectProps={{
-              native: true,
-            }}
-            helperText="Please select your currency"
-            variant="standard"
-          >
-            {currencies.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
             ))}
           </TextField>
         </div>
