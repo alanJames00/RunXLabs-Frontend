7c232778204db0db9b0c421a5efb8a4936bcf098
alanjames00 <alanjames6282@gmail.com>
2023-12-14 21:24:43 +0530

    add output terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import CodeEditor from './components/CodeEditor'
 import NavBar from './components/NavBar'
+import OutputTerminal from './components/OutputTerminal'
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <div className=' flex'>
       <CodeEditor />
+      <OutputTerminal />
+      </div>
     </>
   )
 }
