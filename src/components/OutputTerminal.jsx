e3db2076a3c308060d33dacf68264b1d14cd53aa
alanjames00 <alanjames6282@gmail.com>
2023-12-14 21:18:24 +0530

    add style for terminal

--- src/components/OutputTerminal.jsx
+++ src/components/OutputTerminal.jsx
@@ -3,8 +3,8 @@ export default function OutputTerminal() {
 
     return (
     
-    <div >
-        
-
+    <div className=" text-white pl-2 pt-2" style={{width:'650px', background:'#2a3f57'}}>
+        <p className=" text-green-500">$ output@<span className=" text-yellow-300">runxlabs ~</span></p>
+        <p>{}</p>
     </div>);
 }
