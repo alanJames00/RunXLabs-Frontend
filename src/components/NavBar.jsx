0454ab76c228dcd8685ea23b805ceec3dd8096b6
alanjames00 <alanjames6282@gmail.com>
2023-12-14 21:24:01 +0530

    fix styling for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import ThemeDropdown from "./ThemeDropDown";
 export default function NavBar(){
 
     return (
-        <div className=" h-18 bg-slate-300 flex-row">
+        <div className=" h-18 bg-slate-300 flex-row pt-1">
 
             <div className=" flex justify-center gap-6" >
             <RuntimeDropdown />
